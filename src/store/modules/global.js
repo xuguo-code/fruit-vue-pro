586e01128644f81875209baa44f985870e7ea7bd
xuguo <xuguo@leimingtech.com>
2020-05-12 14:36:11 +0800

    feat(store): 添加全局的主题配置相关的store

--- src/store/modules/global.js
+++ src/store/modules/global.js
@@ -1,15 +1,27 @@
 let state = {
-  routes: []
+  routes: [],
+  theme: {
+    mode: 'light'
+  },
+  menuIsCollapse: false
 }
 
 let mutations = {
   setRoutes(state, r) {
     return (state.routes = r)
+  },
+  setThemeMode(state, mode) {
+    return (state.theme.mode = mode)
+  },
+  setMenuCollapse(state) {
+    return (state.menuIsCollapse = !state.menuIsCollapse)
   }
 }
 
 let getters = {
-  routes: state => state.routes
+  routes: state => state.routes,
+  theme: state => state.theme,
+  menuIsCollapse: state => state.menuIsCollapse
 }
 
 export default {
