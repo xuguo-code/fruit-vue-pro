d49c5f5fdac0b3cd0fb364b7165312c31984d7cd
xuguo <xuguo@leimingtech.com>
2020-09-15 15:29:40 +0800

    feat: 添加面包屑组件

---
+++ src/components/base/FruitBreadcrumb.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <el-breadcrumb>
+      <el-breadcrumb-item
+        v-for="route in breadcrumbData"
+        :key="route.name"
+        :to="{ path: route.path }"
+      >
+        {{ route.title }}
+      </el-breadcrumb-item>
+    </el-breadcrumb>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FruitBreadcrumb',
+  computed: {
+    breadcrumbData: vm => vm.handleOfBreadcrumb(vm.$route.matched || [])
+  },
+  methods: {
+    handleOfBreadcrumb(matched) {
+      return matched.reduce((memo, route) => {
+        /**
+         * {
+         *  path
+         *  title
+         * }
+         */
+        if (!route?.meta?.hideInBreadcrumb) {
+          let title = route?.meta?.title || route.name
+          let path = route?.redirect || route.path
+          if (!path || !name) {
+            return [
+              ...memo,
+              {
+                path,
+                title
+              }
+            ]
+          } else {
+            return memo
+          }
+        }
+      }, [])
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
