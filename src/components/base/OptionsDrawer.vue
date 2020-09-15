dc0f0929a695ae419245ba8c17e9efbb03261331
xuguo <xuguo@leimingtech.com>
2020-09-15 21:46:09 +0800

    perf: 优化弹出操作框

--- src/components/base/OptionsDrawer.vue
+++ src/components/base/OptionsDrawer.vue
@@ -1,19 +1,23 @@
 <template>
-  <div class="drawer-wrap">
-    <el-button :class="['drawer-btn', drawer ? 'open' : '']" @click="drawer = true" type="primary">
-      <svg-icon :icon-name="drawer ? 'menu-open' : 'menu-close'" />
-    </el-button>
-    <el-drawer title="我是标题" :visible.sync="drawer" :with-header="false">
-      <h2>配置</h2>
-      <section class="option-wrap">
-        <div class="option-item">
-          <span>主题色：</span>
-          <el-radio-group v-model="themeMode" size="small">
-            <el-radio-button v-for="t in themes" :key="t" :label="t"></el-radio-button>
-          </el-radio-group>
-        </div>
+  <div :class="{ 'drawer-wrap': true, 'drawer-open': drawer }">
+    <div class="drawer-mask" @click="drawer = !drawer"></div>
+    <div class="drawer-content">
+      <section class="content-main">
+        <h2>配置</h2>
+        <section class="option-wrap">
+          <div class="option-item">
+            <span>主题色：</span>
+            <el-radio-group v-model="themeMode" size="small">
+              <el-radio-button v-for="t in themes" :key="t" :label="t"></el-radio-button>
+            </el-radio-group>
+          </div>
+        </section>
+        <!-- 开启关闭按钮 -->
       </section>
-    </el-drawer>
+      <el-button class="drawer-btn" @click="drawer = !drawer" type="primary">
+        <svg-icon :icon-name="drawer ? 'menu-open' : 'menu-close'" />
+      </el-button>
+    </div>
   </div>
 </template>
 
@@ -22,7 +26,7 @@ export default {
   name: 'OptionsDrawer',
   data() {
     return {
-      drawer: false,
+      drawer: true,
       themes: ['dark', 'light']
     }
   },
@@ -41,20 +45,50 @@ export default {
 
 <style lang="scss" scoped>
 .drawer-wrap {
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  width: 0;
+  z-index: 999;
+  &.drawer-open {
+    width: 100%;
+    .drawer-content {
+      transform: translateX(0);
+    }
+  }
+  .drawer-mask {
+    position: absolute;
+    top: 0;
+    right: 0;
+    width: 100%;
+    height: 100%;
+  }
+  .drawer-content {
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    width: 300px;
+    background: #fff;
+    border-left: 1px solid #e6e6e6;
+    transform: translateX(100%);
+    box-shadow: -6px 0 16px -8px rgba(0, 0, 0, 0.08), -9px 0 28px 0 rgba(0, 0, 0, 0.05),
+      -12px 0 48px 16px rgba(0, 0, 0, 0.03);
+    transition: transform 0.3s cubic-bezier(0.7, 0.3, 0.1, 1),
+      box-shadow 0.3s cubic-bezier(0.7, 0.3, 0.1, 1);
+  }
   .drawer-btn {
     position: absolute;
     top: 50%;
-    right: 0;
+    right: 300px;
     padding: 10px;
     border-radius: 5px 0 0 5px;
-    &.open {
-      right: 30%;
-    }
   }
   .option-wrap {
     height: 100%;
     width: 100%;
-    padding: 10px 40px;
+    padding: 10px 30px;
     text-align: left;
     .option-item {
       display: flex;
