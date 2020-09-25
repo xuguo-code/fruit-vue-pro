144a6c2f007c7637207d6a1035afd3df3005cbd8
xuguo <xuguo@leimingtech.com>
2020-09-25 17:32:25 +0800

    perf: 提升头部的z-index

--- src/layout/baseLayout/FruitHeader.vue
+++ src/layout/baseLayout/FruitHeader.vue
@@ -74,6 +74,7 @@ export default {
   box-shadow: 0 1px 4px rgba(0, 21, 41, 0.08);
   background: $--color-wihte;
   color: $--color-text-main;
+  z-index: 1000;
   .left {
     float: left;
     display: flex;
